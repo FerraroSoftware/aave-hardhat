2b0e3a267cbdf38ce48f59bac37c77d3c95aab79
FerraroSoftware <software.ferraro@gmail.com>
2022-06-12 14:32:47 -0400

    depositing into aave successful now

--- scripts/aaveBorrow.js
+++ scripts/aaveBorrow.js
@@ -1,5 +1,5 @@
 const { getNamedAccounts, ethers } = require("hardhat");
-const { getWeth } = require("../scripts/getWeth");
+const { getWeth, AMOUNT } = require("../scripts/getWeth");
 
 async function main() {
   // protocol treats everything like erc20 token
@@ -15,8 +15,37 @@ async function main() {
   // lending pool: get from ^
   // const lendingPoolAddressProvider = await ethers.getContractAt("", "", account);
   // const lendingPoolAddress = await lendingPoolAddressProvider.getLendingPool();
+
+  // GET AAVE LENDING POOL
   const lendingPool = await getLendingPool(deployer);
   console.log(`LendingPool address ${lendingPool.address}`);
+
+  // depoist!
+  // first we need to approve, get webtoken addr
+  const wethTokenAddress = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+  // APPROVE IS NEEDED!
+  await approveErc20(wethTokenAddress, lendingPool.address, AMOUNT, deployer);
+  console.log("Depositing...");
+  await lendingPool.deposit(wethTokenAddress, AMOUNT, deployer, 0);
+  console.log("Depoisted");
+  // ------------------- END OF DEPOIST --------------------
+}
+
+// spendingaddress - address to approve
+async function approveErc20(
+  erc20Address,
+  spenderAddress,
+  amountToSpend,
+  account
+) {
+  const erc20Token = await ethers.getContractAt(
+    "IERC20",
+    erc20Address,
+    account
+  );
+  const tx = await erc20Token.approve(spenderAddress, amountToSpend);
+  await tx.wait(1);
+  console.log("Approved!");
 }
 
 async function getLendingPool(account) {
