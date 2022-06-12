e21ea36a0e08b1023440699648a91b179a0fdba1
FerraroSoftware <software.ferraro@gmail.com>
2022-06-12 14:11:03 -0400

    add lending pool from aave package

--- scripts/aaveBorrow.js
+++ scripts/aaveBorrow.js
@@ -1,3 +1,4 @@
+const { getNamedAccounts } = require("hardhat");
 const { getWeth } = require("../scripts/getWeth");
 
 async function main() {
@@ -6,7 +7,15 @@ async function main() {
   // eth gets swapped for WETH
 
   await getWeth();
+
+  const { deployer } = await getNamedAccounts();
+  // aave protocol - need abi and addr https://docs.aave.com/developers/v/2.0/deployed-contracts/deployed-contracts
+
+  // lending pool address provider: 0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5
+  // lending pool: get from ^
 }
+
+async function getLendingPool() {}
 
 main()
   .then(() => process.exit(0))
